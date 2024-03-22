f597adc279a13b779c876b5f2dea094d8b3e18e6
Joaquin <joaquiluna07@gmail.com>
2024-03-22 09:42:43 -0300

    Ejercicio 6

---
+++ Ejercicios_POO_python/ejercicio6.py
@@ -0,0 +1,13 @@
+class Familia:
+    def __init__(self, nombre_padre, nombre_madre, hijos=[]):
+        self.nombre_padre = nombre_padre
+        self.nombre_madre = nombre_madre
+        self.hijos = hijos
+    
+    def __str__(self):
+        hijos_str = ", ".join(self.hijos)
+        return f"Padre: {self.nombre_padre}, Madre: {self.nombre_madre}, Hijos: {hijos_str}"
+
+
+familia1 = Familia("Juan", "María", ["Pedro", "Ana", "Luis"])
+print(familia1)
